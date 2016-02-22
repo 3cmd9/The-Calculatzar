fdb831e628900840dccca81d581309abffc71f76
341167849 <341167849@109R030573X9Q22.EDU.YRDSB.NET>
2016-02-22 14:00:21 -0500

    Merge origin/master
    Conflicts:
    	src/calculatzarsep2015/StringCalculator.java

--- src/calculatzarsep2015/StringCalculator.java
+++ src/calculatzarsep2015/StringCalculator.java
@@ -33,17 +33,6 @@ public class StringCalculator {
         }
         return (occurenceCount);
     }
-     
-     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        String data, searchWord;
-        System.out.println("Please enter the info you which to analyze.");
-        data = reader.nextLine();
-        System.out.println("Please enter the keyword to search for.");
-        searchWord = reader.next().toLowerCase();
-        System.out.println("The keyword(" + searchWord + ") appears " + inputCount(data, searchWord) + " time(s).");
-    }
-    
 
     public static void keyWordEncryption() { //should it instead be keywords and non keywords entered separately?
         
@@ -75,10 +64,15 @@ public class StringCalculator {
         for (int x=0;x<nonKeyWords.length;x++){
             //if (!message[x].equalsIgnoreCase(x))
         }
-        
-        
-        
-        
+    }
+     public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        String data, searchWord;
+        System.out.println("Please enter the info you which to analyze.");
+        data = reader.nextLine();
+        System.out.println("Please enter the keyword to search for.");
+        searchWord = reader.next().toLowerCase();
+        System.out.println("The keyword(" + searchWord + ") appears " + inputCount(data, searchWord) + " time(s).");
     }
 
 }
