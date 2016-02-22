2caeda835ae3998c70924c30b1ef8ec597b9ff9a
349173815 <349173815@109R0305737BQ22.EDU.YRDSB.NET>
2016-02-22 13:53:50 -0500

    Merge origin/master
    Conflicts:
    	.gitignore

--- src/calculatzarsep2015/StringCalculator.java
+++ src/calculatzarsep2015/StringCalculator.java
@@ -14,39 +14,38 @@ import java.util.Scanner;
 public class StringCalculator {
 
     public static void keyWordEncryption() { //should it instead be keywords and non keywords entered separately?
-        
+
         Scanner scan = new Scanner(System.in);
-        
+
         System.out.println("Please input the number of keywords");
         int numKeyWords = scan.nextInt(); //holds the number of key words the user intends to use
         String[] keyWords = new String[numKeyWords]; //stores every keyword
         System.out.println("Please input the number of total words in the message (keywords and non-keywords)");
-        int numWords=scan.nextInt(); //stores the total number of words to be in the sentence
-        String[] message=new String[numWords]; //stores the words of the message including keywords to be encoded
+        int numWords = scan.nextInt(); //stores the total number of words to be in the sentence
+        String[] message = new String[numWords]; //stores the words of the message including keywords to be encoded
         String[] nonKeyWords = new String[numWords - numKeyWords]; //stores all the words to be encoded
 
         System.out.println("Please input the keywords");
         //gets keywords
         for (int x = 0; x < numKeyWords; x++) {
-            keyWords[x]=scan.next();
+            keyWords[x] = scan.next();
         }
-        
+
         System.out.println("Please input the nessage");
         //gets whole message
-        for (int x=0;x<message.length;x++){
-            message[x]=scan.next();
+        for (int x = 0; x < message.length; x++) {
+            message[x] = scan.next();
         }
-        
-        
-        
+
         //gets non key words to be shifted
-        for (int x=0;x<nonKeyWords.length;x++){
+        for (int x = 0; x < nonKeyWords.length; x++) {
             //if (!message[x].equalsIgnoreCase(x))
         }
-        
-        
-        
-        
+
     }
 
+    public static String asciiConverter(String word) {
+        return null;
+        
+    }
 }
